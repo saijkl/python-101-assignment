acbc8f392d8d2f994455ebbfea750d195bfb2532
Movva Venkata Sai kiran <72487625+saijkl@users.noreply.github.com>
2020-10-21 23:50:12 +0530

    Update Assignment 2 - Withdrawal,deposit,view balance.py

--- Assignment 2 - Withdrawal,deposit,view balance.py
+++ Assignment 2 - Withdrawal,deposit,view balance.py
@@ -1,36 +1,36 @@
 balance=10000
 print("    ATM    ")
 print("""
-1)        Balance
-2)        Withdraw
-3)        Deposit
-4)        Quit
+1==Balance
+2==Withdraw
+3==Deposit
+4==Quit
 
 
 """)
 Option=int(input("Enter Option: "))
 
 if Option==1:
-    print("Balance  £ ",balance)
+    print("Balance  ",balance)
 
 
 if Option==2:
-    print("Balance  £  ",balance)
+    print("Balance    ",balance)
     Withdraw=float(input("Enter Withdraw amount £ "))
     if Withdraw>0:
         forewardbalance=(balance-Withdraw)
-        print("Foreward Balance  £ ",forewardbalance)
+        print("Foreward Balance   ",forewardbalance)
     elif Withdraw>balance:
         print("No funs in account")
     else:
         print("None withdraw made")
 
 if Option==3:
-    print("Balance  £ ",balance)
+    print("Balance   ",balance)
     Deposit=float(input("Enter deposit amount £ "))
     if Deposit>0:
         forewardbalance=(balance+Deposit)
-        print("Forewardbalance  £ ",forewardbalance)
+        print("Forewardbalance   ",forewardbalance)
     else:
         print("None deposit made")
 
