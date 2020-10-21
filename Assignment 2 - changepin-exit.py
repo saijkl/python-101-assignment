ace257b25033a3b1a2076072765e00fb2f0eefd1
Movva Venkata Sai kiran <72487625+saijkl@users.noreply.github.com>
2020-10-21 22:38:42 +0530

    Add files via upload

---
+++ Assignment 2 - changepin-exit.py
@@ -0,0 +1,37 @@
+class signup:
+    def __init__(self, id, password):
+        self.id = id
+        self.password = password
+        self.error = "Enter a valid username and password"
+    def check(self):
+        if (self.id == log_id and self.password == log_pass):
+            print("Registeration successful")
+        else:
+            print(self.error)    
+            
+log = signup("Sai678",   "Sai67rty")
+log_id = input("Enter your user ID: ")
+log_pass = input("Enter password: ")
+log.check()   
+                
+
+                        
+                
+                        
+                        
+       
+                    
+                        
+                        
+            
+                    
+           
+                    
+                    
+        
+                    
+                     
+                    
+         
+        
+            
